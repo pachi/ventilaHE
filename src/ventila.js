2565861c1058a5fefd4371e9ed5867c80f1f5bf0
Rafael Villar Burke <pachi@ietcc.csic.es>
2024-02-25 20:09:23 +0100

    Más cambios

--- src/ventila.js
+++ src/ventila.js
@@ -51,19 +51,21 @@ const form_fields = [
 
 // Limpia entradas del formulario con valores por defecto
 export function resetform_fields() {
-    form_fields.map(e => {
-        const elem = document.getElementById(e);
-        const reset_value = elem.getAttribute("type") === "text" ? "-" : 0;
-        elem.value = reset_value;
-    });
+  form_fields.map((e) => {
+    const elem = document.getElementById(e);
+    const reset_value = elem.getAttribute("type") === "text" ? "-" : 0;
+    elem.value = reset_value;
+  });
 }
 
 // Traslada valores del formulario a fila de la tabla de tipos
 export function fields2row() {
-  const m = Object.fromEntries(form_fields.map(e => [e, document.getElementById(e).value]))
+  const m = Object.fromEntries(
+    form_fields.map((e) => [e, document.getElementById(e).value])
+  );
 
   // Prueba para sustituir template
-  const res =  `<tr>
+  const res = `<tr>
   <td>${m.nombretipo}</td>
   <td>${m.numtipo}</td>
   <td class="m2">${m.suptipo}</td>
@@ -80,15 +82,14 @@ export function fields2row() {
 
 // Convierte fila de datos a objeto
 export function row2data(row) {
-    
-    console.log("row", row);
+  console.log("row", row);
 
-    // const res = {};
-    // for (const [i, cell] of row.cells.entries()) {
-    //     console.log(form_fields[i], cell)
-    // }
+  // const res = {};
+  // for (const [i, cell] of row.cells.entries()) {
+  //     console.log(form_fields[i], cell)
+  // }
 
-    //TODO: A ver si conseguimos eliminar esto
+  //TODO: A ver si conseguimos eliminar esto
   return _.reduce(
     $(row).find("td"),
     function (accum, value, index) {
@@ -105,44 +106,76 @@ export function row2data(row) {
 
 // Actualiza totales en la tabla
 export function update() {
-    const data_tbody = document.querySelector("#locales tbody");
-    const data_rows = data_tbody.querySelectorAll("tr");
-    const data_new = [...data_rows].map(row => {
-        console.log("data_row", row);
-        row2data(row)
-    })
+  // const data_tbody = document.querySelector("#locales tbody");
+  // const data_rows = data_tbody.querySelectorAll("tr");
+  // const data_new = [...data_rows].map((row) => {
+  //   console.log("data_row", row);
+  //   row2data(row);
+  // });
 
-  const data = _.map($("#locales tbody").find("tr"), row2data);
+  const rows = document.querySelectorAll("#locales tbody tr");
+  const data = Array.from(rows).map(row2data)
 
-  const anumtipos = _.pluck(data, "numtipo");
+  const anumtipos = data.map((i) => i.numtipo);
 
-  const asuptipos = hadamard(anumtipos, _.pluck(data, "suptipo"));
-  const asupcomun = hadamard(anumtipos, _.pluck(data, "supcomun"));
+  const asuptipos = hadamard(
+    anumtipos,
+    data.map((i) => i.suptipo)
+  );
+  const asupcomun = hadamard(
+    anumtipos,
+    data.map((i) => supcomun)
+  );
 
-  const numdormitorios = dot(anumtipos, _.pluck(data, "numdormitorios"));
-  const numestar = dot(anumtipos, _.pluck(data, "numestar"));
-  const numlochum = dot(anumtipos, _.pluck(data, "numlochum"));
-  const numbanos = dot(anumtipos, _.pluck(data, "numbanos"));
-  const supcocina = dot(anumtipos, _.pluck(data, "supcocina"));
+  const numdormitorios = dot(
+    anumtipos,
+    data.map((i) => numdormitorios)
+  );
+  const numestar = dot(
+    anumtipos,
+    data.map((i) => numestar)
+  );
+  const numlochum = dot(
+    anumtipos,
+    data.map((i) => numlochum)
+  );
+  const numbanos = dot(
+    anumtipos,
+    data.map((i) => numbanos)
+  );
+  const supcocina = dot(
+    anumtipos,
+    data.map((i) => supcocina)
+  );
 
-  const suptipos = _.sum(asuptipos);
-  const supcomunes = _.sum(asupcomun);
+  const suptipos = asuptipos.reduce((acc, val) => acc+val, 0);
+  const supcomunes = asupcomun.reduce((acc, val) => acc+val, 0);
   let altmedia =
-    dot(sum_vec(asuptipos, asupcomun), _.pluck(data, "altura")) /
+    dot(
+      sum_vec(asuptipos, asupcomun),
+      data.map((i) => altura)
+    ) /
     (suptipos + supcomunes);
   altmedia = isNaN(altmedia) ? 0 : altmedia;
   const voledif = altmedia * (suptipos + supcomunes);
 
-  $("#voledif").val(voledif);
-  $("#totnumtipo").text(_.sum(anumtipos));
-  $("#totsuptipo").text(Math.round(suptipos * 100) / 100);
-  $("#totaltura").text(Math.round(altmedia * 100) / 100);
-  $("#totsupcomun").text(Math.round(supcomunes * 100) / 100);
-  $("#totnumdormitorios").text(numdormitorios);
-  $("#totnumestar").text(numestar);
-  $("#totnumlochum").text(numlochum);
-  $("#totnumbanos").text(numbanos);
-  $("#totsupcocina").text(Math.round(supcocina * 100) / 100);
+  document.querySelector("#voledif").value = voledif;
+  document.querySelector("#totnumtipo").textContent = anumtipos.reduce(
+    (acc, val) => acc + val,
+    0
+  );
+  document.querySelector("#totsuptipo").textContent =
+    Math.round(suptipos * 100) / 100;
+  document.querySelector("#totaltura").textContent =
+    Math.round(altmedia * 100) / 100;
+  document.querySelector("#totsupcomun").textContent =
+    Math.round(supcomunes * 100) / 100;
+  document.querySelector("#totnumdormitorios").textContent = numdormitorios;
+  document.querySelector("#totnumestar").textContent = numestar;
+  document.querySelector("#totnumlochum").textContent = numlochum;
+  document.querySelector("#totnumbanos").textContent = numbanos;
+  document.querySelector("#totsupcocina").textContent =
+    Math.round(supcocina * 100) / 100;
 
   let vol2009 = ventila2009(data);
   let renh2009 = (3.6 * vol2009) / voledif;
@@ -168,7 +201,7 @@ function ventila2009(data) {
         0.35 * row.supcomun)
     );
   }
-  return _.sum(_.map(data, procesatipo2009)); // l/s
+  return data.map(procesatipo2009).reduce((acc, val)=> acc + val, 0); // l/s
 }
 
 function ventila2015(data) {
@@ -183,7 +216,7 @@ function ventila2015(data) {
     const volminextrloc = Math.max(vol1, volminextr);
     return row.numtipo * (volminextrloc + 0.35 * row.supcomun);
   }
-  return _.sum(_.map(data, procesatipo2015)); // l/s
+  return data.map(procesatipo2015).reduce((acc, val)=> acc + val, 0); // l/s
 }
 
 // Eventos de la interfaz -------------------------------------------------------
